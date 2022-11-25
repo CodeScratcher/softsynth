e3fb30d211cb98b02f59fe453c4df9fc326712b1
Arielle F <bb8andr2d2forlife@gmail.com>
2022-11-24 22:02:19 -0500

    More comprehensive testing and better documentation

--- src/waves.rs
+++ src/waves.rs
@@ -5,6 +5,7 @@
 
 use std::f64::consts::PI;
 
+/// A sinusoidal wave. The first argument is the frequency, the second argument is the amplitude, and the third argument is the x value for the wave.
 pub fn sine(freq: f64, amp: f64, time: f64) -> f64 {
     (time * PI * 2.0 * freq).sin() * amp
 }
@@ -12,6 +13,9 @@ pub fn sine(freq: f64, amp: f64, time: f64) -> f64 {
 #[test]
 fn test_sine() {
     assert_eq!(sine(1.0, 1.0, 0.25), 1.0);
-    assert_eq!(sine(1.0, 1.0, 1.0), 0.0);
+
+    assert_eq!(sine(1.0, 5.0, 0.25), 5.0);
+    assert_eq!(sine(2.0, 5.0, 0.125), 5.0);
+    
     assert_ne!(sine(1.0, 1.0, 0.25), 2.0);
 }
