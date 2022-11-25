968f29bed853428f44a81c34a82c8092e6adca63
Arielle F <bb8andr2d2forlife@gmail.com>
2022-11-24 22:19:32 -0500

    Tested more thoroughly, tests failing

--- src/waves.rs
+++ src/waves.rs
@@ -10,12 +10,47 @@ pub fn sine(freq: f64, amp: f64, time: f64) -> f64 {
     (time * PI * 2.0 * freq).sin() * amp
 }
 
-#[test]
-fn test_sine() {
-    assert_eq!(sine(1.0, 1.0, 0.25), 1.0);
+pub fn square(freq: f64, amp: f64, time: f64) -> f64 {
+    2.0 * amp * (2.0 * (freq * time).floor() - (2.0 * freq * time).floor()) + 1.0
+}
 
-    assert_eq!(sine(1.0, 5.0, 0.25), 5.0);
-    assert_eq!(sine(2.0, 5.0, 0.125), 5.0);
-    
-    assert_ne!(sine(1.0, 1.0, 0.25), 2.0);
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_sine_time() {
+        assert_eq!(sine(1.0, 1.0, 0.0), 0.0);
+        assert_eq!(sine(1.0, 1.0, 0.25), 1.0);
+        
+    }
+
+    #[test]
+    fn test_sine_amp() {
+        assert_eq!(sine(1.0, 5.0, 0.0), 0.0);
+        assert_eq!(sine(1.0, 5.0, 0.25), 5.0);
+    }
+
+    #[test]
+    fn test_sine_freq() {
+        assert_eq!(sine(2.0, 5.0, 0.125), 5.0); 
+    }
+
+    #[test]
+    fn test_square_time() {
+        assert_eq!(square(1.0, 1.0, 0.1), 0.0);
+        assert_eq!(square(1.0, 1.0, 0.7), 1.0);
+    }
+
+    #[test]
+    fn test_square_amp() {
+        assert_eq!(square(1.0, 2.0, 0.1), 0.0);
+        assert_eq!(square(1.0, 2.0, 0.7), 2.0);
+    }
+
+    #[test]
+    fn test_square_freq() {
+        assert_eq!(square(2.0, 1.0, 0.1), 0.0);
+        assert_eq!(square(2.0, 1.0, 0.3), 2.0);
+    }
 }
